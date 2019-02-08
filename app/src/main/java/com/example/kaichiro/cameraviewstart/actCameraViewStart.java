f6cf2f530b57d0f084714986bdc0757ccd4557bb
kaichiro <kcfg23@gmail.com>
2019-02-08 20:47:13 -0200

    Initial commit - iniciando projeto

---
+++ app/src/main/java/com/example/kaichiro/cameraviewstart/actCameraViewStart.java
@@ -0,0 +1,13 @@
+package com.example.kaichiro.cameraviewstart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class actCameraViewStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_camera_view_start);
+    }
+}
