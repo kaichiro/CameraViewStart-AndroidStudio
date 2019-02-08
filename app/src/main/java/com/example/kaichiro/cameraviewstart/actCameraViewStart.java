50fe32bfc6fd7ddaf4efe4689c506c403deb649f
kaichiro <kcfg23@gmail.com>
2019-02-08 21:49:34 -0200

    Updating save photo

--- app/src/main/java/com/example/kaichiro/cameraviewstart/actCameraViewStart.java
+++ app/src/main/java/com/example/kaichiro/cameraviewstart/actCameraViewStart.java
@@ -1,15 +1,29 @@
 package com.example.kaichiro.cameraviewstart;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class actCameraViewStart extends AppCompatActivity {
 
@@ -76,4 +90,49 @@ public class actCameraViewStart extends AppCompatActivity {
                 .positiveText(R.string.label_ok)
                 .show();
     }
+
+    public String saveImage(CameraKitImage cameraKitImage) {
+        File file = null;
+        OutputStream fOut = null;
+        try {
+            file = createImageFile();
+            fOut = new FileOutputStream(file);
+            fOut.write(cameraKitImage.getJpeg());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fOut.close();
+            } catch (IOException e) {
+                Log.d("CAMERA_ERROR", e.getMessage());
+            }
+        }
+
+        return file.getAbsolutePath();
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp =
+                new SimpleDateFormat("yyyyMMdd_HHmmss",
+                        Locale.getDefault()).format(new Date());
+        String imageFileName = "IMG_" + timeStamp + "_";
+        File storageDir =
+                getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".mysecret",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        return imageFile;
+    }
+
+    private void shareImage(String path) {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/jpeg");
+        ContentValues values = new ContentValues();
+        share.putExtra(Intent.EXTRA_STREAM, Uri.parse(path));
+        startActivity(share);
+    }
+
 }
